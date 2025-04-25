684d5822de09aee704c9573094c2bb91816f37ae
SabariKr377 <krsabari1187@gmail.com>
2025-04-25 15:50:41 +0530

    swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,4 @@
-/*import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();*/
-
-
-
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   const port = 3000;
-//   const globalPrefix = '';
-
-//   const config = new DocumentBuilder()
-//     .setTitle('My Project API')
-//     .setDescription('API documentation')
-//     .setVersion('1.0')
-//     .build();
-
-//   const document = SwaggerModule.createDocument(app, config);
-//   SwaggerModule.setup('api', app, document);
-
-//   await app.listen(3000);
-//   console.log(`🚀 Application is running on: http://localhost:${port}${globalPrefix ? '/' + globalPrefix : ''}`);
-// }
-// bootstrap();
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
